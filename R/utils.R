9ee5806600545d013e70aafc7098c6c4cad8822c
Nicholas Sunderland <nicholas.p.sunderland@gmail.com>
2025-03-27 02:34:59 +0000

    reporting output

--- R/utils.R
+++ R/utils.R
@@ -15,10 +15,47 @@ available_data_formats <- function() {
 }
 
 
+
+#' @title List Available Report Templates
+#' @description Scans the package source files for available report templates to write to.
+#' @return A character vector of available report templates
+#' @export
+available_report_templates <- function() {
+  r_dir <- system.file(package = "metaboprep2")
+  templates <- list.dirs(file.path(r_dir, "rmarkdown", "templates"), recursive = FALSE, full.names = FALSE)
+  return(templates)
+}
+
+
 clean_names <- function(names) {
   n <- gsub(" ", "_", names)
   n <- gsub("[-\\.]", "", n)
   tolower(n)
+}
+
+
+check_features_table <- function(tbl) {
+
+  required_columns <- c("feature_id", "platform", "pathway", "derived_feature") #, "feature_names", "comp_id", "kegg", "hmdb")
+
+  missing_columns <- setdiff(required_columns, names(tbl))
+
+  check <- if (length(missing_columns) > 0) FALSE else TRUE
+
+  return(check)
+}
+
+
+check_samples_table <- function(tbl) {
+
+  required_columns <- c("sample_id")
+
+  missing_columns <- setdiff(required_columns, names(tbl))
+
+  check <- if (length(missing_columns) > 0) FALSE else TRUE
+
+  return(check)
+
 }
 
 
@@ -99,7 +136,6 @@ add_layer <- function(current, layer, layer_name, force=FALSE) {
     }
 
   }
-
 
 
   return(current)
