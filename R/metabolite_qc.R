c7242cbc42a005743d0dd5031ffe62f94d25d748
Nicholas Sunderland <nicholas.p.sunderland@gmail.com>
2025-04-11 01:19:51 +0100

    .

--- R/metabolite_qc.R
+++ R/metabolite_qc.R
@@ -41,6 +41,28 @@ method(metabolite_qc, Metabolites) <- function(metabolites, source="raw"){
     cli::cli_alert_success(glue::glue("Normalised raw data."))
   }
 
+  # xenobiotic features to exclude
+  xenobiotic_feats <- character()
+  if (metabolites@xenobiotics_var_exclusion) {
+    cli::cli_alert_info(glue::glue("Applying exclusion of xenobiotic features..."))
+    xenobiotic_feats <- get_features(metabolites, "raw")[grepl("(?i)xenobiotic", pathway), feature_id]
+  } else {
+    cli::cli_alert_info(glue::glue("Skipping exclusion of xenobiotic features..."))
+  }
+  cli::cli_alert_success(glue::glue("Xenobiotic features exclusions assessment complete. ({length(xenobiotic_feats)} features excluded)"))
+
+  # features to include
+  cli::cli_alert_info("Analysing exclusions...")
+  derived_feats <- character()
+  incl_feats    <- setdiff(incl_feats, unlist(metabolites@exclusions[[source]][["features"]]))
+  if (metabolites@derived_var_exclusion) {
+    cli::cli_alert_info(glue::glue("Applying exclusion of derived features..."))
+    derived_feats <- get_features(metabolites, "raw")[derived_feature==TRUE, feature_id]
+  } else {
+    cli::cli_alert_info(glue::glue("Skipping exclusion of derived features..."))
+  }
+  cli::cli_alert_success(glue::glue("Derived features exclusions assessment complete. ({length(derived_feats)} features excluded)"))
+
 
   # run sample summary and feature summary on the raw data
   cli::cli_alert_info(glue::glue("Sample & Feature Summary Statistics for raw data..."))
@@ -69,36 +91,10 @@ method(metabolite_qc, Metabolites) <- function(metabolites, source="raw"){
 
 
 
-  # features to include
-  cli::cli_alert_info("Analysing exclusions...")
-  derived_feats <- character()
-  incl_feats    <- setdiff(incl_feats, unlist(metabolites@exclusions[["qcing"]][["features"]]))
-  if (metabolites@derived_var_exclusion) {
-    cli::cli_alert_info(glue::glue("Applying exclusion of derived features..."))
-    derived_feats <- get_features(metabolites, "raw")[derived_feature==TRUE, feature_id]
-    incl_feats    <- setdiff(incl_feats, derived_feats)
-  } else {
-    cli::cli_alert_info(glue::glue("Skipping exclusion of derived features..."))
-  }
-  cli::cli_alert_success(glue::glue("Derived features exclusions assessment complete. ({length(derived_feats)} features excluded)"))
-
-
-  # xenobiotic features to exclude
-  xenobiotic_feats <- character()
-  if (metabolites@xenobiotics_var_exclusion) {
-    cli::cli_alert_info(glue::glue("Applying exclusion of xenobiotic features..."))
-    xenobiotic_feats <- get_features(metabolites, "raw")[grepl("(?i)xenobiotic", pathway), feature_id]
-    incl_feats       <- setdiff(incl_feats, xenobiotic_feats)
-  } else {
-    cli::cli_alert_info(glue::glue("Skipping exclusion of xenobiotic features..."))
-  }
-  cli::cli_alert_success(glue::glue("Xenobiotic features exclusions assessment complete. ({length(xenobiotic_feats)} features excluded)"))
-
-
 
   # very bad sample missingness
   cli::cli_alert_info(glue::glue("Assessing for extreme sample missingness >=80%..."))
-  dat        <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=incl_feats)
+  dat        <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=setdiff(incl_feats, c(xenobiotic_feats, derived_feats)))
   samplemis  <- missingness(dat, by="row")
   excl_samps <- samplemis[missingness >= 0.8, sample_id]
   metabolites@exclusions[["qcing"]][["samples"]][["extreme_sample_missingness"]] <- unique(c(metabolites@exclusions[["qcing"]][["samples"]][["extreme_sample_missingness"]], excl_samps))
@@ -108,7 +104,7 @@ method(metabolite_qc, Metabolites) <- function(metabolites, source="raw"){
 
   # very bad feature missingness
   cli::cli_alert_info(glue::glue("Assessing for extreme feature missingness >=80%..."))
-  dat        <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=incl_feats)
+  dat        <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=setdiff(incl_feats, xenobiotic_feats))
   featuremis <- missingness(dat, by="column")
   excl_feats <- featuremis[missingness >= 0.8, feature_id]
   metabolites@exclusions[["qcing"]][["features"]][["extreme_feature_missingness"]] <- unique(c(metabolites@exclusions[["qcing"]][["features"]][["extreme_feature_missingness"]], excl_feats))
@@ -119,7 +115,7 @@ method(metabolite_qc, Metabolites) <- function(metabolites, source="raw"){
 
   # re-estimate sample missingness and exclude based on user-defined
   cli::cli_alert_info(glue::glue("Assessing for sample missingness at specified level of >={round(metabolites@sample_missingness*100)}%..."))
-  dat        <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=incl_feats)
+  dat        <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=setdiff(incl_feats, c(xenobiotic_feats, derived_feats)))
   samplemis  <- missingness(dat, by="row")
   excl_samps <- samplemis[missingness >= metabolites@sample_missingness, sample_id]
   metabolites@exclusions[["qcing"]][["samples"]][["user_defined_sample_missingness"]] <- unique(c(metabolites@exclusions[["qcing"]][["samples"]][["user_defined_sample_missingness"]], excl_samps))
@@ -130,7 +126,7 @@ method(metabolite_qc, Metabolites) <- function(metabolites, source="raw"){
 
   # re-estimate feature missingness
   cli::cli_alert_info(glue::glue("Assessing for feature missingness at specified level of >={round(metabolites@feature_missingness*100)}%..."))
-  dat        <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=incl_feats)
+  dat        <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=setdiff(incl_feats, xenobiotic_feats))
   featuremis <- missingness(dat, by="column")
   excl_feats <- featuremis[missingness >= metabolites@feature_missingness, feature_id]
   metabolites@exclusions[["qcing"]][["features"]][["user_defined_feature_missingness"]] <- unique(c(metabolites@exclusions[["qcing"]][["features"]][["user_defined_feature_missingness"]], excl_feats))
@@ -141,7 +137,7 @@ method(metabolite_qc, Metabolites) <- function(metabolites, source="raw"){
 
   # total peak area
   cli::cli_alert_info(glue::glue("Calculating total peak abundance outliers at +/- {metabolites@total_peak_area_sd} Sdev..."))
-  dat <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=incl_feats)
+  dat <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=setdiff(incl_feats, c(xenobiotic_feats, derived_feats)))
   tpa <- total_peak_area(dat)
   tpa[, `:=`(sdev = sd(tpa_total),
              mean = mean(tpa_total))]
@@ -157,7 +153,7 @@ method(metabolite_qc, Metabolites) <- function(metabolites, source="raw"){
   # PCA data
   # first deal with outlier data depending on option selected
   cli::cli_alert_info(glue::glue("Running data outlier analysis at +/- {metabolites@outlier_udist} Sdev..."))
-  dat <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=incl_feats)
+  dat <- get_data(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=setdiff(incl_feats, xenobiotic_feats))
   if (metabolites@outlier_treatment != "leave_be") {
 
     omat <- outlier_detection(dat, nsd = metabolites@outlier_udist, meansd = FALSE, by="column")
@@ -188,18 +184,17 @@ method(metabolite_qc, Metabolites) <- function(metabolites, source="raw"){
   }#end dealing with adjustments pre PCA run
   cli::cli_alert_info(glue::glue("Data outlier analysis complete."))
 
+  # re-identify feature independence and PC outliers (feature_summary will use the current exclusions internally)
+  cli::cli_alert_info(glue::glue("Re-identify feature independence and PC outliers..."))
+  metabolites <- feature_summary(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=setdiff(incl_feats, c(xenobiotic_feats, derived_feats)))
+
+  # identify PC outliers using the newly populated @feature_summary data
+  metabolites <- pc_and_outliers(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=setdiff(incl_feats, xenobiotic_feats))
 
   # run sample summary with the current exclusions internally
   cli::cli_alert_info(glue::glue("Re-running sample summary using current exclusions..."))
   metabolites <- sample_summary(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=incl_feats)
   cli::cli_alert_info(glue::glue("QC sample summary complete."))
-
-  # re-identify feature independence and PC outliers (feature_summary will use the current exclusions internally)
-  cli::cli_alert_info(glue::glue("Re-identify feature independence and PC outliers..."))
-  metabolites <- feature_summary(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=incl_feats)
-
-  # identify PC outliers using the newly populated @feature_summary data
-  metabolites <- pc_and_outliers(metabolites, layer = "qcing", sample_ids=incl_samps, feature_ids=incl_feats)
 
   # extract PCs 1-2 or 1-number of Acceleration factor PCs
   af <- metabolites@acceleration_factor[["qcing"]]
@@ -222,7 +217,7 @@ method(metabolite_qc, Metabolites) <- function(metabolites, source="raw"){
 
   # Make final QC dataset
   cli::cli_alert_info(glue::glue("Creating final QC dataset..."))
-  incl_feats  <- unique(c(incl_feats, xenobiotic_feats)) # add back
+  incl_feats  <- unique(c(incl_feats, derived_feats, xenobiotic_feats)) # add back
   dat <- get_data(metabolites, layer="qcing", sample_ids=incl_samps, feature_ids=incl_feats)
   metabolites@data <- add_layer(current    = metabolites@data,
                                 layer      = dat,
@@ -232,7 +227,8 @@ method(metabolite_qc, Metabolites) <- function(metabolites, source="raw"){
   metabolites@exclusions[["post_qc"]] <- metabolites@exclusions[["qcing"]]
 
   cli::cli_alert_info(glue::glue("Rerunning Sample & Feature Summary Statistics for QC data..."))
-  metabolites <- sample_summary( metabolites, layer="post_qc", sample_ids=setdiff(incl_samps,xenobiotic_feats),
+  metabolites <- sample_summary( metabolites, layer="post_qc",
+                                 sample_ids= incl_samps,
                                  feature_ids=incl_feats, output="object")
   metabolites <- feature_summary(metabolites, layer="post_qc", sample_ids=incl_samps, feature_ids=incl_feats, output="object")
   metabolites <- pc_and_outliers(metabolites, layer="post_qc", sample_ids=incl_samps, feature_ids=incl_feats)
--- R/read_metaboprep1.R
+++ R/read_metaboprep1.R
@@ -2,7 +2,9 @@
 globalVariables(c("CHEMICAL_NAME", "PC_outlier_SD", "PLATFORM", "SUB_PATHWAY",
                   "SUPER_PATHWAY", "feature_missingness", "feature_names",
                   "project", "raw_data", "sample_missingness", "total_peak_area_SD",
-                  "tree_cut_height"), package = "metaboprep2")
+                  "tree_cut_height", "barcode", "combined_id", "derived_features",
+                  "i.barcode2", "label.no.units", "studyId", "study_id", "subclass",
+                  "timepoint", "timepoint2"), package = "metaboprep2")
 
 #' @title Read and Assemble Metaboprep 1 Metabolite Dataset
 #'
@@ -13,6 +15,7 @@ globalVariables(c("CHEMICAL_NAME", "PC_outlier_SD", "PLATFORM", "SUB_PATHWAY",
 #' @param rdata_path Character. Path to the RData file output from the Metaboprep 1 pipeline.
 #' @param log_path Character. Path to the Metaboprep 1 pipeline log file output.
 #' @param clinical_path Character. Path to the clinical metadata file (e.g. age, sex, weight).
+#' @param manifest Character. Path to the NMR manifest prerelease description
 #'
 #' @return A named list with the following components:
 #' \describe{
@@ -33,13 +36,20 @@ globalVariables(c("CHEMICAL_NAME", "PC_outlier_SD", "PLATFORM", "SUB_PATHWAY",
 #' @importFrom data.table fread
 #' @export
 #'
-read_metaboprep1 <- function(rdata_path, log_path, clinical_path) {
+#' @note
+#' Will probably need to rename this to be a specific BBS study import function due to the clinical annotating
+#'
+read_metaboprep1 <- function(rdata_path, log_path, clinical_path, manifest=NULL) {
 
   # testing
   if (FALSE) {
-    rdata_path     <- file.path(Sys.getenv("BBS_METABOLITE_DIR"), "ReportData.Rdata")
-    log_path   <- file.path(Sys.getenv("BBS_METABOLITE_DIR"), "bbs_only_2024_06_25_logfile.txt")
+    rdata_path    <- file.path(Sys.getenv("BBS_NIGHTINGALE_DIR"),  "ReportData.Rdata")
+    log_path      <- file.path(Sys.getenv("BBS_NIGHTINGALE_DIR"),  "bbs_primary_nmr_2023_11_20_logfile.txt")
     clinical_path <- file.path(Sys.getenv("BBS_CLINICAL_DIR"),   "03_sample_clinical_data_all.csv")
+    manifest      <- file.path(Sys.getenv("BBS_MANIFEST_DIR"),   "2023Q2_nmr_annotated_manifest_2023-08-14.csv")
+    # rdata_path    <- file.path(Sys.getenv("BBS_METABOLON_DIR"), "ReportData.Rdata")
+    # log_path      <- file.path(Sys.getenv("BBS_METABOLON_DIR"), "bbs_only_2024_06_25_logfile.txt")
+    #
   }
 
   # read the clinical data
@@ -47,6 +57,7 @@ read_metaboprep1 <- function(rdata_path, log_path, clinical_path) {
     study_id         = "studyId",
     timepoint        = "timepoint",
     client_sample_id = "Client.Sample.ID.",
+    study            = "source_study",
     sex              = "sex",
     age              = "age",
     ethnicity        = "ethnicity",
@@ -57,11 +68,13 @@ read_metaboprep1 <- function(rdata_path, log_path, clinical_path) {
     op_date          = "opdate",
     baseline_to_op   = "baseline_to_op",
     op_to_end        = "op_to_end",
-    baseline_to_end  = "baseline_to_end"
+    baseline_to_end  = "baseline_to_end",
+    site             = "site",
+    time_in_freezer  = "timeinfreezer"
   )
   clinical <- data.table::fread(clinical_path, select = unname(clinical_cols), col.names = names(clinical_cols))
 
-  # read th emetaboprep1 pipeline data
+  # read the metaboprep1 pipeline data
   pipeline <- load(rdata_path)
 
   # read the log file
@@ -87,26 +100,75 @@ read_metaboprep1 <- function(rdata_path, log_path, clinical_path) {
   )
 
   # get the features data
-  features <- raw_data$feature_data |> data.table::as.data.table()
-  features <- features[, list(feature_id      = feature_names,
-                              pathway         = SUPER_PATHWAY,
-                              sub_pathway     = SUB_PATHWAY,
-                              platform        = clean_names(PLATFORM),
-                              chemical_name   = CHEMICAL_NAME,
-                              derived_feature = FALSE)] # will need to adjust if when reading Nightingale
+  features_raw <- raw_data$feature_data |> data.table::as.data.table()
+  if (platform=="Nightingale") {
+    features     <- features_raw[, list(feature_id      = feature_names,
+                                        pathway         = class,
+                                        sub_pathway     = subclass,
+                                        platform        = NA_character_,
+                                        chemical_name   = label.no.units,
+                                        derived_feature = derived_features=="yes")]
+  } else if (platform=="Metabolon") {
+    features     <- features_raw[, list(feature_id      = feature_names,
+                                        pathway         = SUPER_PATHWAY,
+                                        sub_pathway     = SUB_PATHWAY,
+                                        platform        = clean_names(PLATFORM),
+                                        chemical_name   = CHEMICAL_NAME,
+                                        derived_feature = FALSE)]
+  } else {
+    stop("Only Nightingale and Metabolon inputs supported from Metaboprep1 currently.")
+  }
+
 
   # get the samples data (including the platform columns)
-  samples <- raw_data$sample_data |> data.table::as.data.table()
-  data.table::setnames(samples, names(samples), clean_names(names(samples)))
-  sample_cols <- c(sample_id        = "parent_sample_name",
-                   client_sample_id = "client_sample_id",
-                   study            = "source_study",
-                   stats::setNames(unique(features$platform), unique(features$platform)))
-  samples <- samples[, .SD, .SDcols = sample_cols]
-  data.table::setnames(samples, names(samples), names(sample_cols))
+  samples_raw <- raw_data$sample_data |> data.table::as.data.table()
+  data.table::setnames(samples_raw, names(samples_raw), clean_names(names(samples_raw)))
+  if (platform=="Nightingale") {
 
-  # join the clinical to samples
-  samples <- samples[clinical, on = "client_sample_id", nomatch = NULL]
+    sample_cols <- c(sample_id = "sample_id")
+    samples <- samples_raw[, .SD, .SDcols = sample_cols]
+
+    # get the NMR manifest
+    stopifnot("The NMR manifest file must be provided if reading Nightingale NMR prerelease" = !is.null(manifest))
+    manif <- data.table::fread(manifest)
+    manif[, `:=`(barcode2   = sub("\\{\\}", "_", barcode),
+                 timepoint2 = data.table::fcase(grepl("36 months", timepoint), "end",
+                                                grepl("oneyear", timepoint),   "end",
+                                                grepl("(?i)baseline", timepoint),  "Baseline"))]
+    manif[, combined_id := paste0(studyId, "_", timepoint2)]
+    data.table::setcolorder(manif, c("barcode2", "timepoint2","combined_id"))
+
+    clinical[, combined_id := paste0(study_id, "_", timepoint)]
+    clinical[manif, barcode := i.barcode2, on="combined_id"]
+    data.table::setcolorder(clinical, c("barcode"))
+
+    # join the clinical to samples
+    samples_clin <- samples[clinical, on = c("sample_id"="barcode"), nomatch = NULL]
+
+    # missing sample clinical data
+    miss <- setdiff(samples$sample_id, samples_clin$sample_id)
+    if (length(miss) > 0) {
+      msg <- paste0(length(miss), " missing clinical data for samples: ", paste0(miss, collapse=", "))
+      warning(msg)
+    }
+
+    samples <- samples_clin
+
+  } else if (platform=="Metabolon") {
+
+    sample_cols <- c(sample_id        = "parent_sample_name",
+                     client_sample_id = "client_sample_id",
+                     stats::setNames(unique(features$platform), unique(features$platform)))
+    samples <- samples_raw[, .SD, .SDcols = sample_cols]
+    data.table::setnames(samples, names(samples), names(sample_cols))
+
+    # join the clinical to samples
+    samples <- samples[clinical, on = "client_sample_id", nomatch = NULL]
+
+  } else {
+    stop("Only Nightingale and Metabolon inputs supported from Metaboprep1 currently.")
+  }
+
 
   # get the raw data
   data <- raw_data$metabolite_data[samples$sample_id, features$feature_id]
